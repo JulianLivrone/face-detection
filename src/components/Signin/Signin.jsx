82278d6fa77aef35670beadf6747c3a0da478067
JulianLivrone <julian.livrone@gmail.com>
2023-04-25 17:28:43 -0300

    Mostly changes in css

--- src/components/Signin/Signin.jsx
+++ src/components/Signin/Signin.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Loading from "../Loading/Loading";
 import { isEmailValid, isPasswordValid } from "../Utils/Utils";
+import "./Signin.css";
 
 const { REACT_APP_API_URL } = process.env;
 
@@ -44,6 +45,8 @@ const Signin = ({ onRouteChange, loadUser }) => {
           setIsLoading(false);
           if (data.id) {
             loadUser(data);
+            setSignInEmail("");
+            setSignInPassword("");
             onRouteChange("home");
           } else {
             setWrongCredentials(true);
@@ -57,47 +60,46 @@ const Signin = ({ onRouteChange, loadUser }) => {
   };
 
   return (
-    <article className='br3 bab--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-1 center'>
-      <main className='pa4 black-80'>
-        <div className='measure'>
-          <fieldset id='sign_up' className='ba b--transparent ph0 mh0'>
-            <legend className='f1 fw6 ph0 mh0'>Sign In</legend>
-            <div className='mt3'>
-              <label className='db fw6 lh-copy f6' htmlFor='email-address'>
-                Email
-              </label>
-              <input
-                onChange={onEmailChange}
-                className='pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100 b--black'
-                type='email'
-                name='email-address'
-                id='email-address'
-                placeholder='email@address.com'
-              />
-            </div>
-            <div className='mv3'>
-              <label className='db fw6 lh-copy f6' htmlFor='password'>
-                Password
-              </label>
-              <input
-                onChange={onPasswordChange}
-                className='b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100 b--black'
-                type='password'
-                name='password'
-                id='password'
-                placeholder='Minimun 4 characters'
-              />
-            </div>
-          </fieldset>
-          <div className=''>
+    <article className='article'>
+      <main className='main'>
+        <fieldset id='sign_up' className='field_inputs'>
+          <legend className='title'>Sign In</legend>
+          <div className='div_input-text'>
+            <label className='label' htmlFor='email-address'>
+              Email
+            </label>
             <input
-              onClick={onSubmitSignIn}
-              className='b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib'
-              type='submit'
-              value='Sign in'
+              onChange={onEmailChange}
+              className='input-text'
+              type='email'
+              name='email-address'
+              id='email-address'
+              placeholder='email@address.com'
             />
           </div>
+          <div className='div_input-text'>
+            <label className='label' htmlFor='password'>
+              Password
+            </label>
+            <input
+              onChange={onPasswordChange}
+              className='input-text'
+              type='password'
+              name='password'
+              id='password'
+              placeholder='Minimun 4 characters'
+            />
+          </div>
+        </fieldset>
+        <div>
+          <input
+            onClick={onSubmitSignIn}
+            className='input-submit'
+            type='submit'
+            value='Sign in'
+          />
         </div>
+
         {isLoading ? <Loading /> : null}
         {wrongCredentials ? <p>Wrong credentials</p> : null}
         {allInputsAreValid ? null : <p>Fill all inputs correctly</p>}
