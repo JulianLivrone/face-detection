085d9139698683d6051afae53b256873c7ac7541
JulianLivrone <julian.livrone@gmail.com>
2023-04-26 10:20:05 -0300

    CHanges made in Navigation css

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,12 +1,14 @@
 import React from "react";
+import "./Navigation.css";
 
 const Navigation = ({ onRouteChange, isSignedIn }) => {
   if (isSignedIn) {
     return (
-      <nav style={{ display: "flex", justifyContent: "flex-end" }}>
+      <nav className='nav'>
         <p
           onClick={() => onRouteChange("signout")}
-          className='f3 link dim black underline pa3 pointer'>
+          className='f3 link dim black underline pa3 pointer'
+        >
           Sign Out
         </p>
       </nav>
@@ -15,14 +17,10 @@ const Navigation = ({ onRouteChange, isSignedIn }) => {
     return (
       <div>
         <nav style={{ display: "flex", justifyContent: "flex-end" }}>
-          <p
-            onClick={() => onRouteChange("signin")}
-            className='f3 link dim black underline pa3 pointer'>
+          <p onClick={() => onRouteChange("signin")} className='nav-text'>
             Sign In
           </p>
-          <p
-            onClick={() => onRouteChange("register")}
-            className='f3 link dim black underline pa3 pointer'>
+          <p onClick={() => onRouteChange("register")} className='nav-text'>
             Register
           </p>
         </nav>
